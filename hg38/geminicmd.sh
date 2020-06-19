7bf93e4ec38c43049b13c3c3d90f25c6607734cd
erinija <erinija.pranckeviciene@gmail.com>
2020-06-19 08:04:09 +0300

    arranged report fields

--- hg38/geminicmd.sh
+++ hg38/geminicmd.sh
@@ -1,13 +1,40 @@
 #!/bin/sh
 name=$1
-# database full name including .db
-database=$2
+vcfgzfile=$2
+# database will be name.db
+database=${name}.db
+
+# reanotate vcf with vcfanoo
+# this is a temporary solution to make an explicit query to extract just genotype information in a uniform way 
+vcfanno -p 32 -base-path /projects/bcbio/v1.2.0/genomes/Hsapiens/hg38 /projects/Annotations/vcfanno/gnomad_ge.conf ${vcfgzfile} | sed 's/Number=A/Number=1/g' > ${name}.vcf
+#create  db with vcf2db.py 
+# since it is only one sample at the moment, create ped file on the fly
+echo "" >  ${name}.ped
+sed -i '1i#Family_ID\tIndividual_ID\tPaternal_ID\tMaternal_ID\tSex\tPhenotype' ${name}.ped
+sed -i '2iFAMILY\tSAMPLE\t-9\t-9\t0\t0' ${name}.ped
+cat ${name}.ped | sed "s/FAMILY/${name}_fam/" | sed "s/SAMPLE/${name}/"  > SAMPLE.ped
+mv  SAMPLE.ped ${name}.ped
+cat ${name}.ped
+
+echo vcf2db.py
+vcf2db.py ${name}.vcf ${name}.ped ${name}.db
+
+echo gemini query
 # query genotypes and import the genotypes table to the database  
-gemini query --header -q "select variant_id, gts.${name}, gt_types.${name}, gt_phases.${name}, gt_depths.${name}, gt_ref_depths.${name}, gt_alt_depths.${name}, gt_quals.${name}, gt_alt_freqs.${name} from variants" ${database}  > gts.csv
+gemini query --header -q "select variant_id, gts.${name},  gt_depths.${name}, gt_ref_depths.${name}, gt_alt_depths.${name}, gt_quals.${name}, gt_alt_freqs.${name} from variants" ${database} | sed "s/\.${name}//g"  > gts.csv
+
+echo import annotations
 sqlite3 ${database} ".mode tabs" ".headers on" "drop table if exists genotypes;" ".import gts.csv genotypes" 
 sqlite3 ${database} ".mode tabs" ".headers on" "drop table if exists genelevel;" ".import /projects/Annotations/GeneLevel/omim-orpha-ensg-annotations.csv genelevel"
- 
-# run sql script that outputs variant annotation table
-sqlite3 ${database} < report.sql > ${name}-report.csv
 
+echo consolidate annotations 
+# run sql script o consolidate annotations
+sqlite3 ${database} < report.sql 
+echo generate the report
 # Arrange columns properly
+sqlite3 ${database} < fields.sql > ${name}-report.csv
+
+echo clean
+#Remove tables 
+sqlite3 ${database} "drop table if exists genotypes;"  
+sqlite3 ${database} "drop table if exists genelevel;" 
--- hg38/report.sql
+++ hg38/report.sql
@@ -6,14 +6,50 @@
 # https://useast.ensembl.org/info/genome/variation/prediction/predicted_data.html
 ############################################################
 
-## Concatenate all variant effects
+## Concatenate all variant effects separately for refseq and for ensembl
+DROP TABLE IF EXISTS grouped_impacts_refseq;
+CREATE TABLE grouped_impacts_refseq AS select variant_id, COUNT(variant_id) AS Variant_impacts_num_refseq, 
+  GROUP_CONCAT( distinct ( '[' || gene || ';' || impact_so || ';' || impact_severity || ';' || hgvsc || ';' || hgvsp || ']')) AS Grouped_variant_impacts_refseq
+    FROM variant_impacts WHERE NOT INSTR(transcript,"EN") GROUP BY variant_id ;
+
+DROP TABLE IF EXISTS grouped_impacts_enst;
+CREATE TABLE grouped_impacts_enst AS select variant_id, COUNT(variant_id) AS Variant_impacts_num_enst, 
+  GROUP_CONCAT( distinct ( '[' || gene || ';' || impact_so || ';' || impact_severity || ';' || hgvsc || ';' || hgvsp || ']')) AS Grouped_variant_impacts_enst 
+    FROM variant_impacts WHERE INSTR(transcript,"EN") GROUP BY variant_id;
+
+# distinct variant identifiers
+DROP TABLE IF EXISTS var_ids;
+CREATE TABLE var_ids AS SELECT DISTINCT variant_id
+    FROM variant_impacts;
+
+# Join grouped impacts
+DROP TABLE IF EXISTS grouped_impacts_r;
+CREATE TABLE grouped_impacts_r AS SELECT v.variant_id as variant_id,
+  r.Variant_impacts_num_refseq,
+  r.Grouped_variant_impacts_refseq
+FROM var_ids v LEFT JOIN grouped_impacts_refseq r ON v.variant_id=r.variant_id;
+
 DROP TABLE IF EXISTS grouped_impacts;
-CREATE TABLE grouped_impacts AS select variant_id, COUNT(variant_id) AS Variant_impacts_num, GROUP_CONCAT( distinct ( '[' || gene || ';' || impact_so || ';' || impact_severity || ';' || hgvsc || ';' || hgvsp || ']')) AS Grouped_variant_impacts FROM variant_impacts GROUP BY variant_id;
-#SELECT "grouped_impacts ", COUNT(*) FROM grouped_impacts; 
-#SELECT "";
+CREATE TABLE grouped_impacts AS SELECT v.variant_id as variant_id,
+  v.Variant_impacts_num_refseq,
+  v.Grouped_variant_impacts_refseq,
+  r.Variant_impacts_num_enst,
+  r.Grouped_variant_impacts_enst
+FROM grouped_impacts_r v LEFT JOIN grouped_impacts_enst r ON v.variant_id=r.variant_id;
+
+# Join genotypes
+DROP TABLE IF EXISTS grouped_impacts_gts;
+CREATE TABLE grouped_impacts_gts AS SELECT g.*,
+  v.Variant_impacts_num_refseq,
+  v.Grouped_variant_impacts_refseq,
+  v.Variant_impacts_num_enst,
+  v.Grouped_variant_impacts_enst
+FROM genotypes g, grouped_impacts v where g.variant_id=v.variant_id;
 
 
 ## Select fields into the report
+## Select fields from grouped variant effects : refseq and ensembl
+
 DROP TABLE IF EXISTS variant_fields;
 CREATE TABLE variant_fields AS SELECT 
         v.variant_id AS variant_id,
@@ -91,49 +127,32 @@ CREATE TABLE variant_fields AS SELECT
         v.fathmm_mkl_coding_pred AS Fathmm_mkl_coding_pred,
         v.fathmm_mkl_coding_rankscore AS Fathmm_mkl_coding_rankscore,
         v.fathmm_mkl_coding_score AS Fathmm_mkl_coding_score,
-        v.gnomad_ac AS Gnomad_ac,
-        v.gnomad_ac_acr AS Gnomad_ac_afr,
-        v.gnomad_ac_amr AS Gnomad_ac_amr,
-        v.gnomad_ac_asj AS Gnomad_ac_asj,
-        v.gnomad_ac_eas AS Gnomad_ac_eas,
-        v.gnomad_ac_fin AS Gnomad_ac_fin,
-        v.gnomad_ac_nfe AS Gnomad_ac_nfe,
-        v.gnomad_ac_oth AS Gnomad_ac_oth,
-        v.gnomad_ac_popmax AS Gnomad_ac_popmax,
-        v.gnomad_ac_sas AS Gnomad_ac_sas,
-        v.gnomad_af AS Gnomad_af,
-        v.gnomad_af_afr AS Gnomad_af_afr,
-        v.gnomad_af_amr AS Gnomad_af_amr,
-        v.gnomad_af_asj AS Gnomad_af_asj,
-        v.gnomad_af_eas AS Gnomad_af_eas,
-        v.gnomad_af_fin AS Gnomad_af_fin,
-        v.gnomad_af_nfe AS Gnomad_af_nfe,
-        v.gnomad_af_oth AS Gnomad_af_oth,
-        v.gnomad_af_popmax AS Gnomad_af_popmax,
-        v.gnomad_af_sas AS Gnomad_af_sas,
-        v.gnomad_an AS Gnomad_an,
-        v.gnomad_an_anr AS Gnomad_an_afr,
-        v.gnomad_an_amr AS Gnomad_an_amr,
-        v.gnomad_an_asj AS Gnomad_an_asj,
-        v.gnomad_an_eas AS Gnomad_an_eas,
-        v.gnomad_an_fin AS Gnomad_an_fin,
-        v.gnomad_an_nfe AS Gnomad_an_nfe,
-        v.gnomad_an_oth AS Gnomad_an_oth,
-        v.gnomad_an_popmax AS Gnomad_an_popmax,
-        v.gnomad_an_sas AS Gnomad_an_sas,
-        v.gnomad_gc AS Gnomad_gc,
-        v.gnomad_gc_female AS Gnomad_gc_female,
-        v.gnomad_gc_male AS Gnomad_gc_male,
-        v.gnomad_hom AS Gnomad_hom,
-        v.gnomad_hom_female AS Gnomad_hom_female,
-        v.gnomad_hom_male AS Gnomad_hom_male,
-        v.gnomad_popmax AS Gnomad_popmax,
-        v.gnomad_exomes_ac AS Gnomad_exomes_ac,
-        v.gnomad_exomes_af AS Gnomad_exomes_af,
-        v.gnomad_exomes_an AS Gnomad_exomes_an,
-        v.gnomad_genomes_ac AS Gnomad_genomes_ac,
-        v.gnomad_genomes_af AS Gnomad_genomes_af,
-        v.gnomad_genomes_an AS Gnomad_genomes_an,
+        v.hotc_gnomad_exome_ac AS Gnomad_exome_ac,
+        v.hotc_gnomad_exome_an AS Gnomad_exome_an,
+        v.hotc_gnomad_exome_af AS Gnomad_exome_af,
+        v.hotc_gnomad_exome_nhomalt AS Gnomad_exome_nhomalt,
+        v.hotc_gnomad_exome_af_popmax AS Gnomad_exome_af_popmax,
+        v.hotc_gnomad_exome_af_afr AS Gnomad_exome_af_afr,
+        v.hotc_gnomad_exome_af_amr AS Gnomad_exome_af_amr,
+        v.hotc_gnomad_exome_af_asj AS Gnomad_exome_af_asj,
+        v.hotc_gnomad_exome_af_eas AS Gnomad_exome_af_eas,
+        v.hotc_gnomad_exome_af_sas AS Gnomad_exome_af_sas,
+        v.hotc_gnomad_exome_af_fin AS Gnomad_exome_af_fin,
+        v.hotc_gnomad_exome_af_nfe AS Gnomad_exome_af_nfe,
+        v.hotc_gnomad_exome_af_oth AS Gnomad_exome_af_oth,
+        v.hotc_gnomad_genome_ac AS Gnomad_genome_ac,
+        v.hotc_gnomad_genome_an AS Gnomad_genome_an,
+        v.hotc_gnomad_genome_af AS Gnomad_genome_af,
+        v.hotc_gnomad_genome_nhomalt AS Gnomad_genome_nhomalt,
+        v.hotc_gnomad_genome_af_popmax AS Gnomad_genome_af_popmax,
+        v.hotc_gnomad_genome_af_afr AS Gnomad_genome_af_afr,
+        v.hotc_gnomad_genome_af_amr AS Gnomad_genome_af_amr,
+        v.hotc_gnomad_genome_af_asj AS Gnomad_genome_af_asj,
+        v.hotc_gnomad_genome_af_eas AS Gnomad_genome_af_eas,
+        v.hotc_gnomad_genome_af_sas AS Gnomad_genome_af_sas,
+        v.hotc_gnomad_genome_af_fin AS Gnomad_genome_af_fin,
+        v.hotc_gnomad_genome_af_nfe AS Gnomad_genome_af_nfe,
+        v.hotc_gnomad_genome_af_oth AS Gnomad_genome_af_oth,
         v.hg19_chr AS hg19_chr,
         v.hg19_pos AS hg19_pos,
         v.integrated_confidence_value AS Integrated_confidence_value,
@@ -167,53 +186,67 @@ CREATE TABLE variant_fields AS SELECT
         v.domains AS Domains,
         v.hgvsg AS Hgvsg,
         v.dp AS Depth,
-        CAST(g.Variant_impacts_num AS INT) AS Num_of_variant_impacts,
-        g.Grouped_variant_impacts FROM variants v, grouped_impacts g WHERE 
-             v.variant_id=g.variant_id;
+        CAST(r.Variant_impacts_num_refseq AS INT) AS Num_refseq_impacts, 
+         r.Grouped_variant_impacts_refseq AS Refseq_impacts, 
+        CAST(r.Variant_impacts_num_enst AS INT) AS Num_ensembl_impacts,
+         r.Grouped_variant_impacts_enst AS Ensembl_impacts,
+         r.gts,
+         r.gt_depths,
+         r.gt_ref_depths,
+         r.gt_alt_depths,
+         r.gt_quals,
+         r.gt_alt_freqs
+    FROM variants v, grouped_impacts_gts r WHERE 
+             v.variant_id=r.variant_id;
+
 
 #select "Variant fields ",count(*) from variant_fields;
 #select "";
 
-
-# Join fields with variants
-DROP TABLE IF EXISTS variant_fields_gts;
-CREATE TABLE variant_fields_gts AS SELECT g.*, v.* FROM genotypes g, variant_fields v where g.variant_id=v.variant_id;
-
 # Select gene IDs that are from variant table.
-DROP TABLE IF EXISTS vgenes;
-CREATE TABLE vgenes as SELECT distinct VGeneID from variant_fields_gts;
+DROP TABLE IF EXISTS gene_ids;
+CREATE TABLE gene_ids as SELECT distinct VGeneID from variant_fields;
 
 # Join VGeneID with gene level annotations
 DROP TABLE IF EXISTS gl_annotations;
 CREATE TABLE gl_annotations AS SELECT v.VGeneID,gl.* 
-  FROM vgenes v LEFT JOIN genelevel gl ON v.VGeneID=gl.GeneID; 
+  FROM gene_ids v LEFT JOIN genelevel gl ON v.VGeneID=gl.GeneID; 
 
 #select * from gl_annotations limit 5;
 
 DROP TABLE IF EXISTS gl_annotations_grouped;
-CREATE TABLE gl_annotations_grouped AS SELECT VGeneID,
+CREATE TABLE gl_annotations_grouped AS SELECT GeneID,
   GROUP_CONCAT( DISTINCT (Description)) AS Gene_description,
   GROUP_CONCAT( DISTINCT (ENTREZ)) AS ENTREZ,
   GROUP_CONCAT( DISTINCT ('[' || MIM || ';' || OMIM_Phenotypes || ']' )) AS OMIM,
   GROUP_CONCAT( DISTINCT ('[' || Orpha_number || ';' || Orpha_disorder || ';' || Orpha_association || ']' )) AS ORPHA
 FROM gl_annotations GROUP BY GeneID;
 
-#select * from gl_annotations_grouped limit 5;
+#select "gl_annotation_grouped ", count(*) from gl_annotations_grouped;
 
 # The grouped gene level annotations join with variant_fields
 
 DROP TABLE IF EXISTS variant_fields_gts_genelevel;
 CREATE TABLE variant_fields_gts_genelevel AS SELECT v.*, gl.* 
-  FROM  variant_fields_gts v 
-    LEFT JOIN gl_annotations_grouped gl ON v.VGeneID=gl.VGeneID;
+  FROM  variant_fields v 
+    LEFT JOIN gl_annotations_grouped gl ON v.VGeneID=gl.GeneID;
 
-# Add NM
-DROP TABLE IF EXISTS enst;
-CREATE TABLE enst as SELECT DISTINCT Ensembl_transcript_id as vENST FROM  variant_fields_gts_genelevel;
+#select "variant_fields_gts_genelevel ",count(*) from variant_fields_gts_genelevel;
+
+# Add refseq transcript information
+DROP TABLE IF EXISTS enst_ids;
+CREATE TABLE enst_ids as SELECT DISTINCT Ensembl_transcript_id as vENST FROM  variant_fields_gts_genelevel;
+# keep only one identifier if tey are separated by comma
+
+#select "enst_ids ",count(*) from enst_ids;
+
 
 DROP TABLE IF EXISTS enst_nm;
-CREATE TABLE enst_nm as SELECT e.vENST, gl.NM 
- FROM enst e LEFT JOIN genelevel gl ON e.vENST=gl.ENST;
+CREATE TABLE enst_nm as SELECT DISTINCT e.vENST, gl.NM 
+ FROM enst_ids e LEFT JOIN genelevel gl ON e.vENST=gl.ENST;
+
+#select "enst_nm ",count(*) from enst_nm;
+
 
 DROP TABLE IF EXISTS variant_fields_gts_genelevel_nm;
 CREATE TABLE variant_fields_gts_genelevel_nm AS SELECT v.*, e.NM 
@@ -222,18 +255,27 @@ CREATE TABLE variant_fields_gts_genelevel_nm AS SELECT v.*, e.NM
 .mode tabs
 .headers on
 
-## Select fields in a proper order TODO 
-## ( for sample names - how to pass a sample name to the SQL script)
-SELECT DISTINCT * from variant_fields_gts_genelevel_nm;
 
 
-#drop table if exists grouped_impacts;
-#drop table if exists variant_fields;
-#drop table if exists variant_fields_gts;
-#drop table if exists variant_fields_gts_genelevel;
-#drop table if exists vgenes;
-#drop table if exists gl_annotations;
-#drop table if exists gl_annotations_grouped;
+## Select fields in a proper order: separate script 
+## ( TO DO for sample names - how to pass  sample name as a variable to the SQL script)
 
-#drop table if exists genotypes;
-#drop table if exists genelevel;
+#SELECT "Variant fields_gts_genelevel_nm ",count(*) from variant_fields_gts_genelevel_nm;
+
+DROP TABLE IF EXISTS grouped_impacts_refseq;
+DROP TABLE IF EXISTS grouped_impacts_enst;
+DROP TABLE IF EXISTS var_ids;
+DROP TABLE IF EXISTS grouped_impacts_r;
+DROP TABLE IF EXISTS grouped_impacts;
+DROP TABLE IF EXISTS grouped_impacts_gts;
+DROP TABLE IF EXISTS variant_fields;
+DROP TABLE IF EXISTS gene_ids;
+DROP TABLE IF EXISTS gl_annotations;
+DROP TABLE IF EXISTS gl_annotations_grouped;
+DROP TABLE IF EXISTS variant_fields_gts_genelevel;
+DROP TABLE IF EXISTS enst_ids;
+DROP TABLE IF EXISTS enst_nm;
+#DROP TABLE IF EXISTS variant_fields_gts_genelevel_nm;
+
+#DROP TABLE IF EXISTS genotypes;
+#DROP table if exists genelevel;
